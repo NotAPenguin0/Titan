8626630316735f3a2bd67c557ca9f990d49a9da9
NotAPenguin <michiel.vandeginste@gmail.com>
2020-01-07 15:12:41 +0100

    Window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 
+#include <glfw/glfw3.h>
+
 int main() {
-    std::cout << "Titan Project\n";
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize glfw" << std::endl;
+        return 1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    GLFWwindow* win = glfwCreateWindow(800, 600, "Titan playground", nullptr, nullptr);
+
+    glfwMakeContextCurrent(win);
+
+    while(!glfwWindowShouldClose(win)) {
+
+        glfwPollEvents();
+        glfwSwapBuffers(win);
+    }
 }
